bf670542d2450a1bace579e5690ec41715d3b08b
Tulio Perez <saefAllah@gmail.com>
2024-10-05 13:01:47 -0400

    Model requirements

--- auctions/models.py
+++ auctions/models.py
@@ -3,17 +3,51 @@ from django.db import models
 
 
 class User(AbstractUser):
-    # create watchlist object
+    # ********PRIMARY KEY********: AbstractUser: user_id
+    # watchlist
+    # ********FOREIGN KEYS********:
+    # none
     pass
 
-class Item_Listing():
+
+class Auction_Listing():
+    #***PRIMAR KEY***: listing_id
+    # title
+    # description
+    # quantity
+    # price
+    # timestamp listed
+    # closing date
+    # bool open / closed
+    # ********FOREIGN KEYS********:
+    #   user_id
+    #   category_id
+    #   bid_id
     pass
+
 
 class Bid():
+    # ********PRIMARY KEY********: bid_id
+    # timestamp
+    # amount
+    # ********FOREIGN KEYS********:
+    #   listing
+    #   user
     pass
+
 
 class Comment():
+    # ********PRIMARY KEY********: comment_id
+    # timestamp
+    # content
+    # ********FOREIGN KEYS********:
+    #   user_id
+    #   listing_id
     pass
 
-class Watchlist():
+
+class Category():
+    # ********PRIMARY KEY********: category_id
+    #   title
+    #   item count
     pass
